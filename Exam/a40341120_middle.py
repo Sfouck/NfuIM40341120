d1f68d1d4c50a4e4231aff8f443b7a3115d8d463
Sfouck <wjcf129@gmail.com>
2017-04-20 15:56:06 +0800

    20170420 middle exam fix

--- Exam/a40341120_middle.py
+++ Exam/a40341120_middle.py
@@ -26,25 +26,25 @@ for i,v in enumerate(q2):
 print "---第四題---"
 # matplotlib畫圖
 
-
 def string_to_date(date):
     tmp = []
     for i, d in enumerate(date):
         ts = d.split("-")
         tmp.append(datetime.date(int(ts[0]),int(ts[1]),int(ts[2])))
     return tmp
-date = ['2015-1-10', '2015-1-11', '2015-1-12', '2015-1-13', '2015-1-14','2015-1-15', '2015-1-16', '2015-1-17', '2015-1-18', '2015-1-19', '2015-1-20']
+date = ['2015-1-10', '2015-1-11', '2015-1-12', '2015-1-13', '2015-1-14','2015-1-15',
+        '2015-1-16', '2015-1-17', '2015-1-18', '2015-1-19', '2015-1-20']
 temp = np.array([16.7,17.4,17.1,20.3,16.2,16.1,17.5,15.3,16.8,16,18.4])
 plt.plot_date(string_to_date(date),temp,'r-',marker = '.')
 plt.xlabel("Date")
 plt.ylabel("Temperature")
 plt.title("Taipei January Temperature\n40341120")
+plt.show()
 print("date = {}\ntemp = {}".format(string_to_date(date),temp))
 
 print "---第五題---"
 # 亂數矩陣相加
 #random.seed(40341120)
-
 
 def shape(A):
     num_rows = len(A)
@@ -53,23 +53,26 @@ def shape(A):
 def make_matrix(num_rows, num_cols, entry_fn):
     return [[entry_fn(i, j) for j in range(num_cols)]for i in range(num_rows)]
 
-
 def random_number(i, j):
     return random.randint(0,20)
 
-
-def matrix_add(A, B):
+def matrix_cal(A, B,L):
     if shape(A) != shape(B):
         raise ArithmeticError("cannot add matrices with different shapes")
     num_rows, num_cols = shape(A)
     def entry_fn(i, j):
-        return A[i][j] + B[i][j]
+        return L(A[i][j] , B[i][j])
     return make_matrix(num_rows, num_cols, entry_fn)
-
+mAdd = lambda x, y: x+y
+mMinus = lambda x, y : x-y
 matrixA = make_matrix(2, 3, random_number)
 matrixB = make_matrix(2, 3, random_number)
-
-print matrix_add(matrixA,matrixB)
+matrixC = make_matrix(2, 3, random_number)
+print("matrixA = {}".format(matrixA))
+print("matrixB = {}".format(matrixB))
+print("matrixC = {}".format(matrixC))
+print("matrixA - matrixB = {}".format(matrix_cal(matrixA,matrixB,mMinus)))
+print("matrixA + matrixB + matrixC = {}".format(matrix_cal(matrix_cal(matrixA,matrixB,mAdd),matrixC,mAdd)))
 
 print "---第六題---"
 # 填空題
@@ -99,4 +102,3 @@ print "p(get1|get2)/p(get2) = p(get1)p(get2)/p((get2) = p(get1) : ", a1 / n
 
 
 
-plt.show()
